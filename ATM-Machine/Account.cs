6a2f7044a16c3e155ddda7489613b3374e8f2993
hhhoegsted <hh.hoegsted@gmail.com>
2019-08-14 14:42:45 +0200

    Add project files.

---
+++ ATM-Machine/Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Machine
+{
+    class Account
+    {
+        Random r = new Random();
+        private static int numberOfAccounts = 0;
+        private int accountNumber;
+        private string accountOwner;
+        private double balance;
+        private string PIN;
+
+
+        public Account(string accountOwner)
+        {
+            this.accountNumber = ++numberOfAccounts;
+            this.accountOwner = accountOwner;
+            this.balance = 0;
+            this.PIN = r.Next(9999).ToString("0000");
+            Console.WriteLine($"PIN Number for account {accountNumber} is {PIN}");
+        }
+
+        public Account(string accountOwner, double balance) : this(accountOwner)
+        {
+            this.balance = balance;
+        }
+
+        public void Saldo()
+        {
+            Console.WriteLine($"Balance for account {accountNumber} is:\t{this.balance} kr.");
+        }
+
+        private bool CheckAmount(int amount)
+        {
+            return amount % 50 == 0;
+        }
+
+        public void Deposit(int amount)
+        {
+            if (!CheckAmount(amount))
+            {
+                Console.WriteLine("Impossible, must be divisible by 50");
+            }
+            else
+            {
+                this.balance += amount;
+                Console.WriteLine($"You have deposited {amount}, balance is {balance}");
+            }
+            
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (balance < amount)
+            {
+                Console.WriteLine($"Insufficient funds in account {accountNumber}");
+            }
+            else if (!CheckAmount(amount))
+            {
+                Console.WriteLine("Impossible, must be divisible by 50");
+            } else
+            {
+                balance -= amount;
+                Console.WriteLine($"You have withdrawn {amount}, balance is {balance}");
+            }
+        }
+    }
+}
---
+++ ATM-Machine/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Machine
+{
+    class Program
+    {
+        private void ShowPinCheck()
+        {
+            int accountEntered;
+            string pinEntered;
+            Console.WriteLine("Welcome to the ATM, enter your account number");
+            Console.Write("> ");
+            int.TryParse(Console.ReadLine(), out accountEntered);
+            
+        }
+
+        private void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("---------- Welcome to the ATM ----------");
+            Console.WriteLine("Would you like to");
+            Console.WriteLine("1. Withdraw");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Show Balance");
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Account ac = new Account("Hans-Henrik Høgsted");
+            Console.ReadKey();
+            Account ac2 = new Account("Sara Müller", 5000.00);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
